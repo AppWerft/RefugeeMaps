1baee6c1e020fc6b4c40a8fe1ac9c3d431711217
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-11-19 16:47:05 +0100

    adapter added

--- Resources/adapters/refmaps.js
+++ Resources/adapters/refmaps.js
@@ -1,1 +1,26 @@
 const URL = 'http://refugeemaps.eu/_api/hotspots/hamburg.json';
+
+if (!Ti.App.Properties.hasProperty('MARKERS')) {
+	var refmaps = require('adapters/refmaps.default').markers;
+	Ti.App.Properties.setString('MARKERS', JSON.stringify(refmaps));
+}
+
+module.exports = function() {
+	function callbackFn() {
+		if ($.OK)
+			$.OK(JSON.parse(Ti.App.Properties.getString('MARKERS')));
+	};
+	if (!Ti.Network.online)
+		callbackFn();
+
+	var $ = Ti.Network.createHTTPClient({
+		onload : function() {
+			Ti.App.Properties.setString('MARKERS', this.responseText);
+			callbackFn();
+		},
+		onerror : callbackFn
+	});
+	$.open(GET, URL);
+	$.send();
+	return $;
+};
